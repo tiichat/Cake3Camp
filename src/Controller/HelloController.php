f193251678612a6d9f79c963175bf1ff0b8659a4
Nobuharu Kambara <tiichat@outlook.jp>
2017-04-29 01:30:46 +0900

    initialize

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controller;
+
+class HelloController extends AppController {
+    public $name = 'Hello';
+    //public $autoRender = false;
+    public function index() {
+        $this->viewBuilder()->autoLayout(false);
+    }
+}
