bacedc9530ce1db0d5c10005d23fe5c5a99e1044
tiich <tiichat@gmail.com>
2017-05-01 07:44:13 +0900

    phpcs code sniffer 導入

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,15 +1,55 @@
 <?php
+/**
+ * CakePHPの学習用コントロールクラス
+ *
+ * @category Camp
+ * @package  Controller
+ * @author   tiichat <tiichat@gmail.com>
+ * @license  MIT License
+ * @link     https://github.com/tiichat/Cake3Camp
+ */
 namespace App\Controller;
 
-class HelloController extends AppController {
-    public function initialize() {
+/**
+ * CakePHPの学習用コントロールクラス
+ *
+ * @category Camp
+ * @package  Controller
+ * @author   tiichat <tiichat@gmail.com>
+ * @license  MIT License
+ * @link     https://github.com/tiichat/Cake3Camp
+ */
+class HelloController extends AppController
+{
+
+    /**
+     * 初期化
+     *
+     * @return null
+     */
+    public function initialize()
+    {
         $this->name = 'Hello';
         $this->viewBuilder()->autoLayout(true);
         $this->viewBuilder()->Layout('Hello');
     }
-    public function index() {
+
+    /**
+     * /hello を処理
+     *
+     * @return void
+     */
+    public function index()
+    {
     }
-    public function other() {
+
+    /**
+     * /hello/other を処理
+     *
+     * @return void
+     */
+    public function other()
+    {
         $this->viewBuilder()->autoLayout(false);
     }
 }
