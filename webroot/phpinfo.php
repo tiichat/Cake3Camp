8abf952cfac57204b9fe039b6a6d71bb35b563f2
Nobuharu Kambara <tiichat@outlook.jp>
2017-05-16 01:46:07 +0900

    phpcsに従ってソースコードを整形

--- webroot/phpinfo.php
+++ webroot/phpinfo.php
@@ -1,2 +1,12 @@
 <?php
+
+/**
+ * PHPの設定情報を表示する。
+ *
+ * @category View
+ * @package  App
+ * @author   tiichat <tiichat@gmail.com>
+ * @license  MIT License
+ * @link     http://tiichatpub.blogspot.jp
+ */
 phpinfo();
